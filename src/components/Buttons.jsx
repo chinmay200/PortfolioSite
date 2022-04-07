6d2015742a4226c90b7bf525bf77bd8ca2010775
Chinmay Borah <chinmay205765@keshav.du.ac.in>
2022-04-07 17:02:51 +0530

    Added background-gredient

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,8 +1,10 @@
-import '../Styles/button.css'
-import React from 'react'
+import "../Styles/button.css";
+import React from "react";
 
-export default function Buttons({text}) {
+export default function Buttons({ text }) {
   return (
-    <a href='#' className='btn'><h2>{text}</h2></a>
-  )
+    <a href="#" className="btn">
+      {text}
+    </a>
+  );
 }
