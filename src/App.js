0c92c0456b907307a7affca9efbcaa80242c0d20
Chinmay Borah <chinmay205765@keshav.du.ac.in>
2022-04-10 12:12:50 +0530

    Working on navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import Buttons from './components/Buttons';
 
 function App() {
   return (
     <>
       <Navbar/>
+      {/* <Buttons text="Home"></Buttons> */}
     </>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,16 @@
 import '../Styles/nav.css'
+import logo from '../Styles/owl.png'
 import Buttons from './Buttons';
 import React from 'react'
 
 export default function Navbar() {
   return (
-    <nav className='navbar'>
-        
-    </nav>
+    <div className='navbar'>
+      <div className="navigationitems">
+        <div className="logo">
+          <img src={logo} alt="" srcset="" style={{ "width": "3.4em", "height": "3.4em" }} />
+        </div>
+      </div>
+    </div>
   )
 }
