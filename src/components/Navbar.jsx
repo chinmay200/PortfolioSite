860e3166cb8de5b30dc1cf11d2b9a102ee11aa07
Chinmay Borah <chinmay205765@keshav.du.ac.in>
2022-04-10 13:29:55 +0530

    Working on navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,10 +7,19 @@ export default function Navbar() {
   return (
     <div className='navbar'>
       <div className="navigationitems">
+
         <div className="logo">
           <img src={logo} alt="" srcset="" style={{ "width": "3.4em", "height": "3.4em" }} />
         </div>
+
+        <div className="navlinks">
+          <Buttons text = "Home" />
+          <Buttons text = "Projects" />
+          <Buttons text = "Contact me" />
+        </div>
+
       </div>
+      <div className="underline"></div>
     </div>
   )
 }
