f7de596a815f829a7690853ca844a11106c45d84
Chinmay Borah <chinmay205765@keshav.du.ac.in>
2022-04-11 01:26:29 +0530

    Fixed hamburger button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,30 +6,31 @@ import React from 'react'
 
 export default function Navbar() {
   return (
-    <>
-      <div className='navbar'>
-        <div className="navigationitems">
+    <div className='navbar'>
+      <div className="navigationitems">
 
-          <div className="logo">
-            <img src={logo} alt="" srcset="" style={{ "width": "3.2em", "height": "3.2em" }} />
-          </div>
+        <div className="logo">
+          <img src={logo} alt="" srcset="" style={{ "width": "3.2em", "height": "3.2em" }} />
+        </div>
 
-          <div className="mobile-nav">
+        <div className="navlinks">
+          <Buttons text="Home" />
+          <Buttons text="Projects" />
+          <Buttons text="Contact me" />
+        </div>
+
+        <div className="themetoggle">
+          <img src={toggle} alt="" srcset="" style={{ "width": "3em", "height": "3em" }} className="toggle"></img>
+        </div>
+        <div className="mobile-nav">
+          {/* <div className="cont"> */}
+
             <div className="line1"></div>
             <div className="line2"></div>
-          </div>
-          <div className="navlinks">
-            <Buttons text="Home" />
-            <Buttons text="Projects" />
-            <Buttons text="Contact me" />
-            <div className="themetoggle">
-            <img src={toggle} alt="" srcset="" style={{ "width": "3em", "height": "3em" }} className="toggle"></img>
-          </div>
-          </div>
-
+          {/* </div> */}
         </div>
       </div>
-      <div className="underline"></div>
-    </>
+      <div className="underline" style={{ "width": "80vw" }}></div>
+    </div>
   )
 }
