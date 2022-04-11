e3787f9b0d5e20cdf2be3496eb5d2f0c298f4f59
Chinmay Borah <chinmay205765@keshav.du.ac.in>
2022-04-11 22:59:54 +0530

    Fixing navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,6 +14,11 @@ export default function Navbar() {
             <img src={logo} alt="" srcset="" style={{ "width": "3.2em", "height": "3.2em" }} />
           </div>
 
+          <div className="mobile-nav">
+            <div className="line1"></div>
+            <div className="line2"></div>
+          </div>
+
           <div className="navlinks">
             <Buttons text="Home" />
             <Buttons text="Projects" />
@@ -23,10 +28,8 @@ export default function Navbar() {
           <div className="themetoggle">
             <img src={toggle} alt="" srcset="" style={{ "width": "3em", "height": "3em" }} className="toggle"></img>
           </div>
-          <div className="mobile-nav">
-            <div className="line1"></div>
-            <div className="line2"></div>
-          </div>
+
+
         </div>
         <div className="underline" style={{ "width": "80vw" }}></div>
       </div>
