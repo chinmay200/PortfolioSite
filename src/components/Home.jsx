9338480a66fc30f8188667efa9257f674d2a7565
Chinmay Borah <chinmay205765@keshav.du.ac.in>
2022-04-13 22:30:30 +0530

    fixed navbar positions and margins and removed overflow

---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../Styles/home.css'
+
+function Home() {
+    return (
+        <div className='heropage'>
+            Home
+        </div>
+    )
+}
+
+export default Home
