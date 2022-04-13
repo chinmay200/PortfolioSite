a8cd18568f4c49eab33eaff056c2648d42fb45dd
Chinmay Borah <chinmay205765@keshav.du.ac.in>
2022-04-14 01:14:12 +0530

    Started Home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,7 +4,23 @@ import '../Styles/home.css'
 function Home() {
     return (
         <div className='heropage'>
-            Home
+            <div className="intro">
+                <div className="greet-name">
+                    <div className="greet">
+                        <h1>Hi,
+                            <span>I</span>
+                            'm
+                        </h1>
+                    </div>
+                    <div className="name">
+                        <h2>Chinmay</h2>
+                    </div>
+                    <div className="brief">
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Id numquam ab ipsum quis. Cumque quos placeat beatae alias incidunt similique totam explicabo, qui eum ipsa delectus deleniti unde veritatis quisquam.</p>
+                    </div>
+                </div>
+            </div>
+            <div className="underline"></div>
         </div>
     )
 }
