6bfdf36eadb49be371a9bce1959b32d103984e73
Chinmay Borah <chinmay205765@keshav.du.ac.in>
2022-05-02 13:09:08 +0530

    optimised card render

---
+++ src/components/Card.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Card(props) {
+    return (
+        <div className="card">
+            <div className="circle">
+                <h2>{props.title}</h2>
+                <h1>#{props.id}</h1>
+            </div>
+            <div className="content">
+                <p>{props.desc}</p>
+                <a className='hyperbtn' href={props.link} target={"_blank"} rel="noreferrer">Project Link</a>
+
+            </div>
+        </div>
+    )
+}
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,11 +1,33 @@
 import React, { useEffect, useState } from 'react'
 import '../Styles/projects.css'
 import HashLoader from "react-spinners/HashLoader";
+import Card from './Card';
 
 function Projects() {
 
   const height = 80;
   const [loader, setLoader] = useState(false);
+
+  const projects = [
+    {
+      id: 1,
+      title: "Simple whether",
+      desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis dolore minus ut eligendi nesciunt tempora recusandae reprehenderit. Corporis, ullam szaepe.",
+      link: "https://github.com/chinmay200/SimpleWheather"
+    },
+    {
+      id: 2,
+      title: "Rock-Paper-Scissors",
+      desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis dolore minus ut eligendi nesciunt tempora recusandae reprehenderit. Corporis, ullam szaepe.",
+      link: "https://github.com/chinmay200/Rock-Paper-Scissors"
+    },
+    {
+      id: 3,
+      title: "Stackoverflown-Clone",
+      desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis dolore minus ut eligendi nesciunt tempora recusandae reprehenderit. Corporis, ullam szaepe.",
+      link: "https://github.com/chinmay200/Stackoverflown-Clone"
+    }
+  ]
 
   useEffect(() => {
     setLoader(true);
@@ -21,43 +43,15 @@ function Projects() {
           <div className="projects">
             <div className="head">
               <h1>
-                My project<span>s</span> 
+                My project<span>'s</span>
               </h1>
             </div>
             <div className="container">
-              <div className="card">
-                <div className="circle">
-                  <h2>Simple Wheather</h2>
-                  <h1>#1</h1>
-                </div>
-                <div className="content">
-                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis dolore minus ut eligendi nesciunt tempora recusandae reprehenderit. Corporis, ullam saepe.</p>
-                  <a className='hyperbtn' href="https://github.com/chinmay200/SimpleWheather" target={"_blank"} rel="noreferrer">Project Link</a>
-
-                </div>
-              </div>
-
-              <div className="card">
-                <div className="circle">
-                  <h2>Rock-Paper-Sci</h2>
-                  <h1>#2</h1>
-                </div>
-                <div className="content">
-                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis dolore minus ut eligendi nesciunt tempora recusandae reprehenderit. Corporis, ullam saepe.</p>
-                  <a className='hyperbtn' href="https://github.com/chinmay200/Rock-Paper-Scissors" target={"_blank"} rel="noreferrer">Project Link</a>
-                </div>
-              </div>
-
-              <div className="card">
-                <div className="circle">
-                  <h2>StackOverflow-Clone</h2>
-                  <h1>#3</h1>
-                </div>
-                <div className="content">
-                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis dolore minus ut eligendi nesciunt tempora recusandae reprehenderit. Corporis, ullam saepe.</p>
-                  <a className='hyperbtn' href="https://github.com/chinmay200/Stackoverflown-Clone" target={"_blank"} rel="noreferrer">Project Link</a>
-                </div>
-              </div>
+              {projects.map((project) => {
+                return (
+                  <Card key={project.id} title={project.title} id={project.id} desc={project.desc} link={project.link} />
+                )
+              })}
             </div>
           </div>
         </div>
